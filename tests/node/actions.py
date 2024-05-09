41e787fbaa19cc6c80fd90439ab35ac29a862a9c
Arnab Ghose <arnab.ghose128@gmail.com>
2024-05-09 14:57:13 +0530

    changed base port for ens

--- tests/node/actions.py
+++ tests/node/actions.py
@@ -41,7 +41,7 @@ def quorum_config(node_config: dict, skip_adding_quorums: bool = False, create_q
 
 
 def get_base_ports():
-    base_ens_server = 40000
+    base_ens_server = 23000
     base_grpc_port = 10500
 
     return base_ens_server, base_grpc_port
